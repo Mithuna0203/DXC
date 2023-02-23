e66bbb97e5dd3049045626eae08659eef45391bd
Mithuna0203 <126164025+Mithuna0203@users.noreply.github.com>
2023-02-23 17:41:41 +0530

    Add files via upload

---
+++ CreateDirStructure.bat
@@ -0,0 +1,18 @@
+cd c:\
+mkdir PowerShell
+cd PowerShell
+mkdir MyDir1
+mkdir MyDir2
+mkdir MyDir3
+cd MyDir1
+mkdir MyDir11
+mkdir MyDir12
+cd ..
+cd MyDir2
+mkdir MyDir21
+mkdir MyDir22
+cd ..
+cd MyDir3
+mkdir MyDir31
+mkdir MyDir32
+
